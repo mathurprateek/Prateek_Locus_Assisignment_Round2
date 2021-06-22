8386d6e1779b3c15b2db57dde428d752cba87c97
mathurprateek <37469411+mathurprateek@users.noreply.github.com>
2021-06-23 01:40:09 +0530

    Update task1_locus
    Enhanced value error condition check for latitude and longitude

--- Task1_Locus/task1_locus.py
+++ Task1_Locus/task1_locus.py
@@ -32,24 +32,30 @@ def index():
 # creating class resource, extending flask_restful's Resource for API method execution
 class AtmPrs(Resource):
     def get(self):  # runs when API is called with GET method request
-        arg = req_param.parse_args()  # fetching request parsed values
-        lat, lon = arg["lat"], arg["lon"]  # unpacking latitude and longitude values
-        tzf_str = TimezoneFinder().timezone_at(lng=lon, lat=lat)  # identifying timezone as per coordinates passed
-        tz = pytz.timezone(tzf_str)  # timezone object for identified timezone
-        dt_tz = dt.now(tz)  # current time in target timezone
-        tz_datetime = dt(dt_tz.year, dt_tz.month, dt_tz.day, 4, tzinfo=tz)  # setting target timezone's 04 AM datetime
-        utc_dst_delta = dt_tz.utcoffset().total_seconds() - tz_datetime.utcoffset().total_seconds()  # UTC offset delta
-        unix_datetime_dict = {}  # dictionary to store unix datetime
-        datetime_dict = {}  # dictionary to store human readable datetime
-
-        def last_three_days_unix_time_four_am(today_tz_datetime):  # function to add last 3 day's to unix dictionary
-            for i in range(1, 4):
-                datetime_dict[str(today_tz_datetime - timedelta(days=i))[0:19]] = ""
-                unix_datetime_dict[int((today_tz_datetime - timedelta(days=i, seconds=utc_dst_delta)).timestamp())] = ''
-
-        last_three_days_unix_time_four_am(tz_datetime)
-
         try:
+            arg = req_param.parse_args()  # fetching request parsed values
+            if (-90 <= arg["lat"] <= 90) and (-180 <= arg["lon"] <= 180):
+                lat, lon = arg["lat"], arg["lon"]  # unpacking latitude and longitude values
+            else:
+                raise ValueError
+            tzf_str = TimezoneFinder().timezone_at(lng=lon, lat=lat)  # identifying timezone as per coordinates passed
+            tz = pytz.timezone(tzf_str)  # timezone object for identified timezone
+            dt_tz = dt.now(tz)  # current time in target timezone
+            # setting target timezone's 04 AM datetime
+            tz_datetime = dt(dt_tz.year, dt_tz.month, dt_tz.day, 4, tzinfo=tz)
+            # UTC offset delta
+            utc_dst_delta = dt_tz.utcoffset().total_seconds() - tz_datetime.utcoffset().total_seconds()
+            unix_datetime_dict = {}  # dictionary to store unix datetime
+            datetime_dict = {}  # dictionary to store human readable datetime
+
+            def last_three_days_unix_time_four_am(today_tz_datetime):  # function to add last 3 day's to unix dictionary
+                for i in range(1, 4):
+                    datetime_dict[str(today_tz_datetime - timedelta(days=i))[0:19]] = ""
+                    unix_datetime_dict[int((today_tz_datetime -
+                                            timedelta(days=i, seconds=utc_dst_delta)).timestamp())] = ''
+
+            last_three_days_unix_time_four_am(tz_datetime)
+
             for k in unix_datetime_dict.keys():  # looping over unix timestamps stored
                 res = requests.get(
                     f'https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={k}'
@@ -58,9 +64,7 @@ class AtmPrs(Resource):
             for k, v in zip(datetime_dict.keys(), unix_datetime_dict.values()):
                 datetime_dict[k] = v  # setting values in human readable dictionary
         except ValueError:
-            pass
-        except KeyError as ke:
-            return {"Caught an KeyError": ke}
+            return jsonify({"Status Code": 400, "ValueError": "Check the Value of Latitude and Longitude"})
         except Exception as exc:
             return {"Got an Exception": exc}
         return jsonify({"Atmospheric pressure at 4am for the past 3 days": datetime_dict,
